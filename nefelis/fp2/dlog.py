56dd9737f5d895a5e61e5d59bd0668313eb62eb2
Rémy O <remyoudompheng@gmail.com>
2025-09-20 23:24:19 +0200

    feat: more complete implementation of fp2 dlog
    This includes:
    - ability to recurse on f-side prime ideals (not just g-side)
    - CLI interface improvements
    - adjustment to sieving parameters
    - multithreaded smoothing step
    - fixed recursion when q=qr

--- nefelis/fp2/dlog.py
+++ nefelis/fp2/dlog.py
@@ -5,6 +5,7 @@ Currently only computations in the norm 1 subgroup (of order p+1)
 modulo the largest prime factor are implemented.
 """
 
+import argparse
 import json
 import logging
 import math
@@ -12,6 +13,7 @@ import pathlib
 import random
 import sys
 import time
+from concurrent.futures import ThreadPoolExecutor
 
 import flint
 
@@ -26,10 +28,31 @@ DEBUG_SMALL_LOGS = False
 # Check intermediate logarithms (cheap)
 DEBUG_EXTRA_CHECKS = True
 
+DEBUG_TIMINGS = True
+
 
 def main():
-    workdir = pathlib.Path(sys.argv[1])
-    args = sys.argv[2:]
+    argp = argparse.ArgumentParser()
+    argp.add_argument(
+        "-v",
+        action="store_true",
+        help="Verbose logging",
+    )
+    argp.add_argument("WORKDIR")
+    argp.add_argument("ARGS", nargs="+", help="list of pairs x,y (for elements x+iy)")
+    args = argp.parse_args()
+
+    if args.v:
+        logging.getLogger().setLevel(level=logging.DEBUG)
+    else:
+        logging.getLogger().setLevel(level=logging.INFO)
+
+    main_impl(args)
+
+
+def main_impl(args):
+    workdir = pathlib.Path(args.WORKDIR)
+    args = args.ARGS
 
     with open(workdir / "args.json") as fd:
         doc = json.load(fd)
@@ -93,14 +116,18 @@ PARAMS = [
     # initial threshold during rational reconstruction sieve (as a fraction of n)
     # MAX_QBITS: max prime size in initial decomposition (must be under 64 bits)
     # COFACTOR_BITS: cofactor size during descent
-    (100, 0.5, 40, 10),
-    (140, 0.6, 60, 15),
+    # COFACTOR_BITS2: cofactor size for large q descent
+    (100, 0.7, 40, 10, 20),
+    (120, 0.65, 50, 15, 40),
+    (140, 0.6, 60, 15, 50),
+    (160, 0.50, 60, 15, 50),
 ]
 
 
 class Descent:
     def __init__(self, dlogs, logbase, n, f, g, z, ell):
         self.dlogs = dlogs
+        self.pending = set()
 
         self.logbase = logbase
         self.n = n
@@ -115,12 +142,14 @@ class Descent:
         self.Fp = flint.fmpz_mod_ctx(n)
 
         nbits = n.bit_length()
-        _, ratio, self.MAX_QBITS, COFACTOR_BITS = min(
+        _, ratio, self.MAX_QBITS, COFACTOR_BITS, COFACTOR_BITS2 = min(
             PARAMS, key=lambda p: abs(p[0] - nbits)
         )
         self.zthreshold = int(ratio * nbits)
 
         self.dlogs["GEN"] = 1
+        if "CONSTANT" not in self.dlogs:
+            self.dlogs["CONSTANT"] = 0
 
         fls, frs = [], []
         gls, grs = [], []
@@ -137,11 +166,31 @@ class Descent:
                 grs.append(lr)
                 self.glogs.add((l, lr))
         self.g_primes = list(zip(gls, grs))
+        # Also add more small primes to make sieving easier
+        self.g_primes_extended = self.g_primes.copy()
+        glextra = integers.smallprimes(max(gls))
+        g_set = set(gls)
+        extra = 0
+        for l in glextra:
+            if l not in g_set:
+                for r, _ in flint.nmod_poly(g, l).roots():
+                    self.g_primes_extended.append((l, int(r)))
+                    gls.append(l)
+                    grs.append(int(r))
+                    extra += 1
+        logger.info(f"Added {extra} small Kg primes with unknown logs for sieving")
+        self.g_primes.sort()
+        self.g_primes_extended.sort()
+
+        # MAX_FBITS: allowed size for special-q on the other side (Kf)
+        self.MAX_FBITS = max(max(fls), max(gls)).bit_length() + 5
+
         gsize = max(gi.bit_length() for gi in g)
         LOGWIDTH = 14
         # Size of f is 4 * (LOGWIDTH + logq / 2)
         fthreshold = 4 * LOGWIDTH
         gthreshold = gsize + 2 * LOGWIDTH - COFACTOR_BITS
+        gthreshold2 = gsize + 2 * LOGWIDTH - COFACTOR_BITS2
 
         logger.info(f"Prime ideals f:{len(fls)} g:{len(gls)}")
         # Siever for q less than 30 bits
@@ -163,7 +212,7 @@ class Descent:
             self.g,
             gls,
             grs,
-            gthreshold - 5,
+            (gthreshold + gthreshold2) // 2,
             LOGWIDTH,
             # We want f(x,y) to factor completely
             self.f,
@@ -178,7 +227,7 @@ class Descent:
             self.g,
             gls,
             grs,
-            gthreshold - 10,
+            gthreshold2,
             LOGWIDTH,
             # We want f(x,y) to factor completely
             self.f,
@@ -195,6 +244,9 @@ class Descent:
             return rs[0][0], rs[0][0]
         else:
             return rs[0][0], rs[1][0]
+
+    factor_stats = 0.0
+    factor_stats_decay = 0.8
 
     def _factorg(self, x, y):
         """
@@ -220,8 +272,17 @@ class Descent:
             ideals.append(((l, r1), e))
             ideals.append(((l, r2), e))
 
-        facs = integers.factor(self.norm(x, y))
+        t0 = time.monotonic()
+        facs = integers.factor_smooth(self.norm(x, y), self.MAX_QBITS - 5)
+        if DEBUG_TIMINGS:
+            dt = time.monotonic() - t0
+            self.factor_stats *= self.factor_stats_decay
+            self.factor_stats += dt
+
         for l, e in facs:
+            if not flint.fmpz(l).is_prime():
+                return None, None
+            l, e = int(l), int(e)
             r = x * pow(y, -1, l) % l if y % l else l
             if (l, r) in self.glogs:
                 ideals.append(((l, r), e))
@@ -253,6 +314,13 @@ class Descent:
         """
         n = self.n
         z = self.z
+        factorpool = ThreadPoolExecutor(8)
+
+        def factor_rs(args):
+            Rx, Ry, Sx, Sy = args
+            facs_u, xu = self._factorg(Rx, Ry)
+            facs_v, xv = self._factorg(Sx, Sy)
+            return facs_u, xu, facs_v, xv, args
 
         for i in range(1_000_000):
             if i > 0:
@@ -296,10 +364,17 @@ class Descent:
                 # Use sieve to find (R1 x + R2 y, S1 x + S2 y) smooth
                 # Smoothness should be understood in field K(g)
                 ls, lr1, lr2 = [], [], []
-                for l, lr in sorted(self.glogs):
+                for l, lr in sorted(self.g_primes_extended):
                     # R1 x + R2 y = (a1 x + a2 y) - (b1 x + b2 y) z
                     # This is the same transformation as sieving q-lattices
-                    # x/y = (b2 r - a2)(a1 - r b1) mod l
+                    # x/y = (b2 r - a2)/(a1 - r b1) mod l
+                    ls.append(l)
+                    if lr == l:
+                        r1 = b2 * pow(-b1, -1, l) % l if b1 % l else l
+                        r2 = d2 * pow(-d1, -1, l) % l if d1 % l else l
+                        lr1.append(r1)
+                        lr2.append(r2)
+                        continue
                     if (a1 - lr * b1) % l == 0:
                         r1 = l
                     else:
@@ -308,7 +383,6 @@ class Descent:
                         r2 = l
                     else:
                         r2 = (d2 * lr - c2) * pow(c1 - lr * d1, -1, l) % l
-                    ls.append(l)
                     lr1.append(r1)
                     lr2.append(r2)
 
@@ -329,9 +403,12 @@ class Descent:
                 )
 
                 n_fractions = 0
-                for q, qr in zip(ls[10:40], lr1[10:40]):
+                for q, qr in [(1, 0)] + list(zip(ls[:40], lr1[:40])):
+                    if q == qr:
+                        continue
                     reports = sieve.sieve(q, qr)
-                    logger.info(f"Vector sieve {q},{qr}: {len(reports)} reports")
+                    logger.debug(f"Vector sieve {q},{qr}: {len(reports)} reports")
+                    rs = []
                     for sx, sy in reports:
                         if math.gcd(sx, sy) != 1:
                             continue
@@ -341,8 +418,9 @@ class Descent:
                         assert (Rx - Ry * z) / (Sx - Sy * z) == x
                         # print(Rx, Ry)
                         # print(Sx, Sy)
-                        facs_u, xu = self._factorg(Rx, Ry)
-                        facs_v, xv = self._factorg(Sx, Sy)
+                        rs.append((Rx, Ry, Sx, Sy))
+
+                    for facs_u, xu, facs_v, xv, rs in factorpool.map(factor_rs, rs):
                         if facs_u is None or facs_v is None:
                             continue
 
@@ -355,7 +433,11 @@ class Descent:
 
                         # print("R", Rx, Ry)
                         # print("S", Sx, Sy)
-                        yield facs, cofacs, (Rx, Ry, Sx, Sy)
+                        yield facs, cofacs, rs
+
+                    if DEBUG_TIMINGS:
+                        avg = self.factor_stats * (1 - self.factor_stats_decay)
+                        logger.debug(f"Average factor duration {avg * 1000:.3f}ms")
 
     def log(self, x0):
         """
@@ -367,6 +449,7 @@ class Descent:
         logger.info(
             f"Trying to decompose {x0} into small primes ({self.MAX_QBITS} bits)"
         )
+        t0 = time.monotonic()
         for facs, cofacs, RS in self.smooth_candidates(x0):
             # Now x = product(facs) * xu / xv
             if best is None:
@@ -385,15 +468,17 @@ class Descent:
 
             if best is not None:
                 max_cofac = best[1][-1][0][0]
-                if max_cofac.bit_length() < self.MAX_QBITS:
+                if max_cofac.bit_length() <= self.MAX_QBITS:
                     break
 
+        dt = time.monotonic() - t0
+        logger.info(f"Smoothing finished in {dt:.2f}s")
         facs, cofacs, RS = best
         # x0 = product(facs) * x
         log = sum(e * self.dlogs[keyg(*f)] for f, e in facs)
         # coell = (self.n - 1) // self.ell
         for (_l, _lr), _e in cofacs:
-            logger.info(f"Recurse into small prime {_l},{_lr}")
+            logger.debug(f"Recurse into small prime {_l},{_lr}")
             llog = self.smalllog(_l, _lr)
             log += _e * llog
 
@@ -426,35 +511,35 @@ class Descent:
         vg = u * x * x + v * x * y + w * y * y
         return vf, vg
 
-    def smalllog(self, q: int, qr: int) -> int:
+    def smalllog(self, q: int, qr: int, side: str = "g") -> int:
         """
         Compute logarithm of a "small" prime using sieving.
         """
         assert flint.fmpz(q).is_prime()
-        key = f"g_{q}_{qr}"
+        key = f"{side}_{q}_{qr}"
         if key in self.dlogs:
             logger.info(f"Using known log({key}) = {self.dlogs[key]}")
             return self.dlogs[key]
 
         # If a single sieve is not enough, use larger q-lattices
-        q2s = self.g_primes[:100]
-        if q.bit_length() < 30:
-            q2s = [(1, 0)] + q2s
-
+        if q == qr:
+            # Don't try composite lattices with a root at infinity
+            q2s = [(1, 0)]
+        elif q.bit_length() < 30:
+            q2s = [(1, 0)] + self.g_primes[:100]
+        else:
+            q2s = self.g_primes[:500]
         for qq, qqr in q2s:
             if qqr == qq:
                 continue  # it's complicated
             t = time.monotonic()
-            qcomp = q * qq
-            qrcomp = (qqr * q * pow(q, -1, qq) + qr * qq * pow(qq, -1, q)) % qcomp
-            assert qrcomp % q == qr and qrcomp % qq == qqr, (
-                q,
-                qr,
-                qq,
-                qqr,
-                qcomp,
-                qrcomp,
-            )
+            if (qq, qqr) != (1, 0):
+                qcomp = q * qq
+                qrcomp = (qqr * q * pow(q, -1, qq) + qr * qq * pow(qq, -1, q)) % qcomp
+                assert qrcomp % q == qr and qrcomp % qq == qqr,  (qrcomp, q, qr, qq, qqr)
+            else:
+                qcomp, qrcomp = q, qr
+
             if qcomp.bit_length() <= 29:
                 reports = self.siever.sieve(qcomp, qrcomp)
             elif qcomp.bit_length() <= 35:
@@ -462,19 +547,22 @@ class Descent:
             else:
                 reports = self.sieverhuge.sievelarge(qcomp, qrcomp)
             dt = time.monotonic() - t
-            logger.info(f"Found {len(reports)} sieve results (q={q}*{qq}) in {dt:.3}s")
-            time.sleep(1)
+            logger.debug(f"Found {len(reports)} sieve results (q={q}*{qq}) in {dt:.3}s")
 
             # Store: a list of factors, list of missing primes, the largest missing prime
             best: tuple[list, list, int] | None = None
 
-            n, z = self.n, self.z
+            z = self.z
+            n_good = 0
             for x, y in reports:
                 x, y = int(x), int(y)
                 if math.gcd(x, y) != 1:
                     continue
                 vf, vg = self.fg(x, y)
-                facf = integers.factor(abs(vf))
+                facf = integers.factor_smooth(
+                    abs(vf) // q if side == "f" else abs(vf),
+                    self.MAX_FBITS - 5
+                )
 
                 good = True
                 # The relation is (see linalg.py) is
@@ -483,30 +571,40 @@ class Descent:
                     ("CONSTANT", -1),
                 ]
                 dlogf = 0
+                missingf = False
                 for _l, _e in facf:
+                    if not flint.fmpz(_l).is_probable_prime():
+                        good = False
+                        break
                     _r = x * pow(y, -1, _l) % _l if y % _l else _l
-                    if (key := f"f_{_l}_{_r}") not in self.dlogs:
+                    if (
+                        key := f"f_{_l}_{_r}"
+                    ) not in self.dlogs and _l.bit_length() > self.MAX_FBITS:
                         good = False
                         break
                     else:
                         facs.append((key, _e))
+                        if key not in self.dlogs:
+                            missingf = True
+                            continue
                         dlogf += _e * self.dlogs[key]
 
-                if vg % q != 0:
-                    raise ArithmeticError(
-                        f"Unexpected g(x,y) not divisible by {q=}, overflow?"
-                    )
+                if side == "g":
+                    if vg % q != 0:
+                        raise ArithmeticError(
+                            f"Unexpected g(x,y) not divisible by {q=}, overflow?"
+                        )
 
                 if not good:
                     continue
 
                 # Since f(x,y) is completely factored, the logarithm of x-yz is known
-                if DEBUG_EXTRA_CHECKS:
+                if DEBUG_EXTRA_CHECKS and side == "g" and not missingf:
                     xy1 = (x - y * z) ** self.projexp
                     assert xy1 == self.logbase**dlogf
 
                 # z = g(z)/u = f(z)
-                facg = integers.factor(abs(vg) // q)
+                facg = integers.factor(abs(vg) // q if side == "g" else abs(vg))
                 for _l, _e in facg:
                     _r = x * pow(y, -1, _l) % _l if y % _l else _l
                     key = f"g_{_l}_{_r}"
@@ -516,31 +614,45 @@ class Descent:
                 worst = 0
                 for k, _ in facs:
                     if k not in self.dlogs:
-                        assert k.startswith("g_")
                         _l, _lr = parse_key(k)
                         worst = max(worst, _l)
-                        missing.append((_l, _lr))
+                        missing.append((_l, _lr, k[0]))
+                if any(ideal in self.pending for ideal in missing):
+                    continue
+                if worst.bit_length() > self.MAX_QBITS:
+                    continue
 
+                # Both f-primes and g-primes are good
+                n_good += 1
                 keep = (
                     best is None
                     or not missing
                     or (best[2] > q and worst < q)
                     or (len(missing) <= len(best[1]) and max(missing) < max(best[1]))
+                    # We don't like missing f-primes
+                    or (
+                        any(k[2] == "f" for k in best[1])
+                        and all(k[2] == "g" for k in missing)
+                    )
                 )
                 if not keep:
                     continue
+                if side == "f":
+                    # Flip sign
+                    facs = [(_l, -_e) for _l, _e in facs]
                 facs_str = "*".join(f"{_l}^{_e}" for _l, _e in facs)
                 if not missing:
-                    logger.info(f"Good report {x},{y}")
-                    logger.info(f"g_{q}_{qr} = {facs_str}")
+                    logger.debug(f"Good report {x},{y}")
+                    logger.debug(f"{side}_{q}_{qr} = {facs_str}")
                     log = sum(e * self.dlogs[k] for k, e in facs) % self.ell
-                    logger.info(f"log(g_{q}_{qr}) = {log}")
-                    self.dlogs[f"g_{q}_{qr}"] = log
+                    logger.info(f"log({side}_{q}_{qr}) = {log}")
+                    self.dlogs[f"{side}_{q}_{qr}"] = log
                     return log
                 else:
                     logger.debug(f"Good report {x},{y} worst={worst}")
                     best = facs, missing, worst
 
+            logger.debug(f"{n_good} reports with no large missing primes")
             if best is not None:
                 break
 
@@ -548,32 +660,36 @@ class Descent:
             raise ValueError(f"failed to find any relation for ideal {q},{qr}")
 
         facs, missing, _ = best
-        logger.info(f"Best relation {q} = {facs_str}")
-        logger.info(f"Best relation is missing primes {missing}")
+        key = f"{side}_{q}_{qr}"
+        logger.debug(f"Best relation {key} = {facs_str}")
+        logger.debug(f"Best relation is missing primes {missing}")
         log = 0
         for k, e in facs:
-            if k not in self.dlogs:
-                assert k.startswith("g_")
-                _l, _lr = parse_key(k)
-                if _l > q:
-                    logger.info(f"Recurse into LARGER prime {_l},{_lr}")
-                else:
-                    logger.info(f"Recurse into small prime {_l},{_lr}")
-                log += e * self.smalllog(_l, _lr)
-            else:
+            if k in self.dlogs:
                 if DEBUG_SMALL_LOGS and k.startswith("g_"):
                     _l, _lr = parse_key(k)
                     xxx = self.smalllog(_l, _lr)
                     assert xxx == self.dlogs[k]
                 log += e * self.dlogs[k]
 
+        missing_str = "*".join(f"{_l}^{_e}" for _l, _e in facs if _l not in self.dlogs)
+        logger.info(f"log({key}) = {log} + log({missing_str})")
+        for k, e in facs:
+            if k not in self.dlogs:
+                self.pending.add((q, qr))
+                assert k.startswith(("f_", "g_"))
+                _l, _lr = parse_key(k)
+                if _l > q:
+                    logger.debug(f"Recurse into LARGER prime {k}")
+                else:
+                    logger.debug(f"Recurse into small prime {k}")
+                log += e * self.smalllog(_l, _lr, side=k[0])
+
         log %= self.ell
-        logger.info(f"Recursive log(g_{q}_{qr}) = {log}")
-        self.dlogs[f"g_{q}_{qr}"] = log
+        logger.info(f"Recursive log({key}) = {log}")
+        self.dlogs[key] = log
         return log
 
 
 if __name__ == "__main__":
-    logging.getLogger().setLevel(level=logging.DEBUG)
-    logging.getLogger("vulkan").setLevel(level=logging.DEBUG)
     main()
