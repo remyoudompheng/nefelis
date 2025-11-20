b4c0c570801e2eb4d24abb30016cace0e93a3b1f
Rémy O <remyoudompheng@gmail.com>
2025-11-20 05:42:14 +0100

    feat: introduce sieve benchmark suite

--- nefelis/sieve.py
+++ nefelis/sieve.py
@@ -98,3 +98,126 @@ def eta(
 
 
 __all__ = [Siever, LineSiever, LineSiever2, eta]
+
+
+def benchmark():
+    """
+    Run sieves with a specific synthetic parameter set.
+    """
+    import logging
+    import time
+
+    from nefelis.integers import smallprimes
+    from nefelis.vulkan import gpu_cores
+
+    logging.getLogger().setLevel(level=logging.INFO)
+
+    # We choose 2 linear polynomials for this benchmark (should not affect performance)
+    u1, v1 = 2**256 + 1, 3**192 + 1
+    u2, v2 = 5**128 + 1, 7**96 + 1
+    cores = gpu_cores()
+
+    primes = smallprimes(20_000_000)
+    froots = [pow(u1, -1, l) * -v1 % l if u1 % l else l for l in primes]
+    groots = [pow(u2, -1, l) * -v2 % l if u2 % l else l for l in primes]
+
+    def runbench1(B1f: int, B1g: int, thr1: int, thr2: int, I: int):
+        nonlocal u1, v1, u2, v2
+        ls = []
+        rs = []
+        ls2 = []
+        rs2 = []
+        for p, r1, r2 in zip(primes, froots, groots):
+            if p < B1f:
+                ls.append(p)
+                rs.append(r1)
+            if p < B1g:
+                ls2.append(p)
+                rs2.append(r2)
+
+        # prewarm GPU
+        S = Siever([v1, u1], ls, rs, thr1, I, [v2, u2], ls2, rs2, thr2, outsize=2**24)
+        idx1, idx2 = 10, 10 + cores
+        for _ in range(2):
+            for q, qr in zip(ls[idx1:idx2], rs[idx1:idx2]):
+                xys = S.sieve(q, qr)
+
+        t0 = time.monotonic()
+        total = 0
+        for q, qr in zip(ls[idx1:idx2], rs[idx1:idx2]):
+            xys = S.sieve(q, qr)
+            total += len(xys)
+        dt = time.monotonic() - t0
+
+        area = 2 ** (2 * I + 1) * cores
+        speed = area / dt
+        logging.info(
+            f"Siever      B1={B1f // 1000}k/{B1g // 1000}k thresholds {thr1}/{thr2} "
+            f"shape {2 ** (I - 10)}k x {2 ** (I + 1)}: {speed * 1e-9:.2f}G/s, {total} results"
+        )
+
+    def runbench2(B1f: int, B1g: int, thr1: int, thr2: int, W: int, H: int):
+        nonlocal u1, v1, u2, v2
+        ls = []
+        rs = []
+        ls2 = []
+        rs2 = []
+        for p, r1, r2 in zip(primes, froots, groots):
+            if p < B1f:
+                ls.append(p)
+                rs.append(r1)
+            if p < B1g:
+                ls2.append(p)
+                rs2.append(r2)
+
+        # prewarm GPU
+        S = LineSiever2([v1, u1], [v2, u2], ls, rs, thr1, ls2, rs2, thr2, W, H, True)
+        idx1, idx2 = 10, 10 + cores
+        for _ in range(2):
+            for q, qr in zip(ls[idx1:idx2], rs[idx1:idx2]):
+                _, xys = S.sieve(q, qr)
+
+        t0 = time.monotonic()
+        total = 0
+        for q, qr in zip(ls[idx1:idx2], rs[idx1:idx2]):
+            _, xys = S.sieve(q, qr)
+            total += len(xys)
+        dt = time.monotonic() - t0
+
+        area = 2 * W * H * cores
+        speed = area / dt
+        logging.info(
+            f"LineSiever2 B1={B1f // 1000}k/{B1g // 1000}k thresholds {thr1}/{thr2} shape {2 * W >> 10}k x {H}: {speed * 1e-9:.2f}G/s, {total} results"
+        )
+
+    # Special shapes (deg3)
+    runbench1(2000_000, 300_000, 120, 30, 14)
+    runbench2(2000_000, 300_000, 120, 30, 16384, 16384)
+
+    # Small shapes
+    runbench1(500_000, 200_000, 60, 70, 14)
+    runbench1(1000_000, 500_000, 80, 80, 14)
+    runbench1(5000_000, 3000_000, 100, 90, 14)
+    runbench1(15000_000, 10000_000, 120, 100, 14)
+
+    # Small shapes
+    runbench2(500_000, 200_000, 60, 70, 16384, 16384)
+    runbench2(1000_000, 500_000, 80, 80, 16384, 16384)
+    runbench2(5000_000, 3000_000, 100, 90, 16384, 16384)
+    runbench2(15000_000, 10000_000, 120, 100, 16384, 16384)
+
+    # Larger skew
+    runbench2(500_000, 200_000, 60, 70, 160 << 10, 1638)
+    runbench2(1000_000, 500_000, 80, 80, 160 << 10, 1638)
+    runbench2(5000_000, 3000_000, 100, 90, 160 << 10, 1638)
+    runbench2(15000_000, 10000_000, 120, 100, 160 << 10, 1638)
+
+    # Extreme skew
+    runbench2(500_000, 200_000, 60, 70, 1 << 20, 256)
+    runbench2(1000_000, 500_000, 80, 80, 1 << 20, 256)
+    runbench2(5000_000, 3000_000, 100, 90, 1 << 20, 256)
+    runbench2(15000_000, 10000_000, 120, 100, 1 << 20, 256)
+
+
+if __name__ == "__main__":
+    benchmark()
